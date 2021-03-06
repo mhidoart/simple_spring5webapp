c44aee8713e62612f612a6e21d37ab230fc769ed
ASSABBANE Mehdi <mhidoart@gmail.com>
2021-03-07 00:13:42 +0100

    adding a constructor to Author class

--- src/main/java/guru/springframewor/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframewor/spring5webapp/domain/Author.java
@@ -23,6 +23,11 @@ public class Author {
         this.books = books;
     }
 
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
     public String getFirstName() {
         return firstName;
     }
